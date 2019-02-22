d1f231519689c20d111ced570d29b1abd67d5680
jacKlinc <jackharding321@gmail.com>
2019-02-22 12:10:41 +0000

    Ready to run

--- Modbus/esm_read.py
+++ Modbus/esm_read.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
-from pymodbus.client.sync import ModbusTcpClient as ModbusClient
-from smbus2 import SMBus, SMBusWrapper
+from pymodbus.modbus_client.sync import ModbusTcpClient as ModbusClient
+#from smbus2 import SMBus, SMBusWrapper
 import time
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import ctypes_callable
 from influxdb import InfluxDBClient
 '''
@@ -16,26 +16,23 @@ Watts Average
 VA Average
 
 Averaging to take place over a 5 minute interval
-Volts are in register 0x1200 (measured in 0.1 V)
-kWatts are in register 0x1204 (measured in 0.1kW)
-kVAs are in register 0x1208 (measured in 0.1kVA)
-kWhs are in register 0x120b (measured in 0.1kWh)'''
+Volts in register 	0x1200 (measured in 0.1 V)
+kWatts in register 	0x1204 (measured in 0.1kW)
+kVAs in register 	0x1208 (measured in 0.1kVA)
+kWhs in register 	0x120b (measured in 0.1kWh)'''
 
-AveragingInterval = 300 			# set to 300 for 5 minutes
+AveragingInterval = 10 			
 
-GPIO.setwarnings(False)
-I2C_bus = smbus2.SMBusWrapper(1)			# for RPI version 1, use "bus = smbus.SMBus(0)"
-I2C_Address = 0x04					# This is the address we setup in the Arduino Program
+#GPIO.setwarnings(False)
+#I2C_bus = smbus2.SMBusWrapper(1)			# for RPI version 1, use "bus = smbus.SMBus(0)"
+#I2C_Address = 0x04					# This is the address we setup in the Arduino Program
 
 ESM_IP_ADDRESS='192.168.255.1'		# connect to the ESM
-client = ModbusClient(ESM_IP_ADDRESS)
+modbus_client = ModbusClient(ESM_IP_ADDRESS)
 
 influx_client = InfluxDBClient(host='localhost', port=8086)
 influx_client.switch_database('Modbus1')
 
-def writeToAnarduino(AvgV,VMax,VMin,AvgkW,AvgkVA,kWh):
-
-=======
 ''' Protocol is as follows:
 	Command number = 1 : Write reading
  '[' = start of message
@@ -46,22 +43,23 @@ def writeToAnarduino(AvgV,VMax,VMin,AvgkW,AvgkVA,kWh):
  abcdef = 3 digit reading for AvgkVA,
  abcdef = 6 digit reading for kWh
  ']' = end of message '''
- 
+
+'''def writeToAnarduino(AvgV,VMax,VMin,AvgkW,AvgkVA,kWh):
 	Message="["+format(AvgV,"03")+","+format(VMax,"03")+","+format(VMin,"03")+"," + format(AvgkW,"03")+","+format(AvgkVA,"03")+","+format(kWh,"06")+"]"
 	print Message
 	I2C_bus.write_i2c_block_data(I2C_Address, 1, [ord(a) for a in Message]) # write_i2c_block_data(addr, addr_offset, data)
 
 def wpiopriteI2CNumber(value):
 	data = [ (value >> 24), ( (value >> 16) & 0xff), ((value >> 8) & 0xff), (value & 0xff)]
-        return I2C_bus.write_i2c_block_data(I2C_Address, 1, data)
+    return I2C_bus.write_i2c_block_data(I2C_Address, 1, data)
         
 def readljhI2CNumber():
-        number = I2C_bus.read_byte(I2C_Address)
-        return number
-
+	number = I2C_bus.read_byte(I2C_Address)
+	return number
+'''
 
 def readESMInputRegisterInt32(Address):
-	response = client.read_input_registers(Address,2)
+	response = modbus_client.read_input_registers(Address,2)
 	if (response is not None):
 		ReturnValue = ((response.getRegister(0) << 16) + response.getRegister(1))
 		if (ReturnValue & 0x80000000): 				# MSB set so negative
@@ -71,7 +69,7 @@ def readESMInputRegisterInt32(Address):
 		return -1
 
 def readESMkW1():
-    	return readESMInputRegisterInt32(0x1204)		#	return 012
+    return readESMInputRegisterInt32(0x1204)		#	return 012
 def readESMV1():
 	return readESMInputRegisterInt32(0x1200)		#	return 123
 def readESMkWh1():
@@ -88,6 +86,20 @@ def readESMkWh2():
 def readESMkVA2():
 	return readESMInputRegisterInt32(0x1219)
 
+def influx_write(meas, tag, meas_type, meas_value)
+	influx_client.writePoints([
+		{
+		measurement: meas, 					# Modbus_D
+		tags:   { Device: tag }, 			# PM311
+		fields: { M_Type: meas_type, Value: meas_value },
+		}									# is it voltage/power, its value
+	])
+	return True
+	#try:
+	'''except Exception as exc:
+		print "Error"
+		return False'''
+
 count = 0
 SecondCounter = 0
 kWSum = 0
@@ -96,24 +108,19 @@ VMax = -1
 VMin = 100000
 VoltageSum = 0
 
-def influx_write(meas, tag, meas_type, meas_value)
-	influx_client.writePoints([
-		{
-		  measurement: meas, 				# Modbus_D
-		  tags:   { Device: tag }, 			# PM311
-		  fields: { M_Type: meas_type, Value: meas_value },
-		}									# is it voltage/power, its value
-	])
-	return True
-
 while True:
 	try:		
 		kW = readESMkW2()
 		kWSum = kWSum + kW
+		print(kWSum) 
+		influx_write("Modbus_D", "PM311", "kW", kWSum)
+		'''
 		V = int(readESMV2()/10)
 		VoltageSum = VoltageSum + V
+
 		kVA = readESMkVA2()
 		kVASum = kVASum + kVA
+
 		kWh = readESMkWh2()
 		if (V > VMax):
 			VMax = V
@@ -125,22 +132,23 @@ while True:
 			AveragekW = kWSum / SecondCounter
 			AveragekVA = kVASum / SecondCounter
 			AverageVoltage = VoltageSum / SecondCounter
-			writeToAnarduino(AverageVoltage, VMax, VMin, AveragekW, AveragekVA, kWh)
+			#writeToAnarduino(AverageVoltage, VMax, VMin, AveragekW, AveragekVA, kWh)
 			kWSum = 0
 			kVASum = 0
 			VoltageSum = 0
 			SecondCounter = 0
 			VMax = -1 				# set unreachably low value for VMax for next averaging period
 			VMin = 100000 			# set unreachably high value for VMin for next averaging period
+			'''
 			
 
 	except Exception as inst:
 		print type(inst)
 		print inst
 		print "Communications error.  Continuing"
-# Attempt to reset the anarduino here by driving a port pin high to low to high
-# The DTR/Reset pin is connected to pin 7 (GPIO4) of the RPi3 header
-		'''print "Resetting anarduino"
+		'''# Attempt to reset the anarduino here by driving a port pin high to low to high
+		# The DTR/Reset pin is connected to pin 7 (GPIO4) of the RPi3 header
+		print "Resetting anarduino"
 		PIN = 7
 		GPIO.setmode(GPIO.BOARD)
 		GPIO.setup(PIN,GPIO.OUT)
@@ -148,7 +156,7 @@ while True:
 		GPIO.output(PIN,GPIO.LOW)
 		time.sleep(1)
 		GPIO.output(PIN,GPIO.HIGH)'''
-		time.sleep(5)
+		time.sleep(0.5)
 		# Reset averaging
 		kWSum = 0
 		kVASum = 0
@@ -156,4 +164,3 @@ while True:
 		SecondCounter = 0
 		VMax = -1
 		VMin = 100000
-
