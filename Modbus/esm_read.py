5f504c811ddb78965904327e9aa4a44c36cb38b3
jacKlinc <jackharding321@gmail.com>
2019-03-15 21:37:19 +0000

    esm_read now pulls from both hall sensors

--- Modbus/esm_read.py
+++ Modbus/esm_read.py
@@ -1,51 +1,49 @@
 #!/usr/bin/python
 from pymodbus.client.sync import ModbusTcpClient as ModbusClient
-#from influxdb import InfluxDBClient
+from influxdb import InfluxDBClient	
 import time
-#import ctypes_callable
 import ctypes
-'''
-This program reads data from an Electric "Smart" Meter (ESM) and outputs
-it to an Anarduino over the I2C bus.  The Arduino values to be sent:
-Average Voltage,
-Voltage Max,
-Voltage Min,
-Watt Hours,
-Watts Average
-VA Average
+					
+m_client = ModbusClient('192.168.255.1')		# slave IP
+i_client = InfluxDBClient(host='localhost', port=8086) # set Grafana data source to this
+i_client.switch_database('test3')			# change to database name
 
-Averaging to take place over a 5 minute interval
-Volts in register 	0x1200 (measured in 0.1 V)
-kWatts in register 	0x1204 (measured in 0.1kW)
-kVAs in register 	0x1208 (measured in 0.1kVA)
-kWhs in register 	0x120b (measured in 0.1kWh)'''
-
-AveragingInterval = 300 			
-ESM_IP_ADDRESS='192.168.255.1'		# connect to the ESM
-modbus_client = ModbusClient(ESM_IP_ADDRESS)
-
-# influx_client = InfluxDBClient(host='localhost', port=8086)
-# influx_client.switch_database('Modbus1')
+avg_interval = 0.02 
+volt_sum = 0
+volt_sum2 = 0
+count = 0
+sec_counter = 0
+kW_sum = 0
+kW_sum2 = 0
+kW_avg = 0
+kVA_sum = 0
+kVA_sum2 = 0
+kVA_avg = 0
+v_max = -1
+v_min = 100000
+V_avg = 0
 
 def readESMInputRegisterInt32(Address):
-	response = modbus_client.read_input_registers(Address,2)
+	response = m_client.read_input_registers(Address, 2)
 	if (response is not None):
 		ReturnValue = ((response.getRegister(0) << 16) + response.getRegister(1))
 		if (ReturnValue & 0x80000000): 				# MSB set so negative
 			ReturnValue = -0x100000000 + ReturnValue
-		return ReturnValue 							#((response.getRegister(0) << 16) + response.getRegister(1))
+		return ReturnValue 						#((response.getRegister(0) << 16) + response.getRegister(1))
 	else:
 		return -1
 
+# Meter 1 read functions
 def readESMkW1():
-    return readESMInputRegisterInt32(0x1204)		#	return 012
+    return readESMInputRegisterInt32(0x1204)	
 def readESMV1():
-	return readESMInputRegisterInt32(0x1200)		#	return 123
+	return readESMInputRegisterInt32(0x1200)		
 def readESMkWh1():
-	return readESMInputRegisterInt32(0x120b) 		#	return 234
+	return readESMInputRegisterInt32(0x120b) 		
 def readESMkVA1():
-	return readESMInputRegisterInt32(0x1208) 		#	return 567
+	return readESMInputRegisterInt32(0x1208) 		
 
+# Meter 2 read functions
 def readESMkW2():
 	return readESMInputRegisterInt32(0x1215)
 def readESMV2():
@@ -55,72 +53,88 @@ def readESMkWh2():
 def readESMkVA2():
 	return readESMInputRegisterInt32(0x1219)
 
-'''def influx_write(meas, tag, meas_type, meas_value):
-	influx_client.writePoints([
-		{
-		measurement: meas, 					# Modbus_D
-		tags:   { Device: tag }, 			# PM311
-		fields: { M_Type: meas_type, Value: meas_value },
-		}									# is it voltage/power, its value
-	])
-	return True
-	#try:
-	except Exception as exc:
-		print "Error"
-		return False
-'''
-count = 0
-SecondCounter = 0
-kWSum = 0
-kVASum = 0
-VMax = -1
-VMin = 100000
-VoltageSum = 0
+# Influx write to database function
+def influx_write(meas, value_type, value):	# passed measurement, 
+	points = [{
+            "measurement": meas,
+            "fields": {
+                "Value": value
+			},
+			"tags": {
+				"M_Type": value_type
+			}
+        }
+    ]
+    i_client.write_points(points)
 
-while True:
-	#try:		
+def v_max_min(volt, volt_min, volt_max):
+	if (volt > volt_max):
+		volt_max = volt
+	if (volt < volt_min):
+		volt_min = volt
+	v=volt
+	v_min=volt_min
+	v_max=volt_max
+	return 1
+
+def averages(met, avg, sum, val_type):
+	if (sec_counter >= avg_interval) :
+		avg = sum / sec_counter
+		influx_write("Meter" + met, val_type, avg)
+		
+		sum = 0
+        sec_counter = 0
+        v_max = -1         # set unreachably low value for v_max for next averaging period
+        v_min = 100000                      # set unreachably high value for v_min for next averaging period    
+		return 1
+
+while True:	
+	# Meter 1
+	kW = readESMkW1()
+	kW_sum += kW
+	influx_write("Meter 1", "kW", kW_sum)
+	
+	v = int(readESMV1()/10)
+	volt_sum += v
+	influx_write("Meter 1", "V", volt_sum)
+	
+	kVA = readESMkVA1()
+	kVA_sum += kVA
+	influx_write("Meter 1", "kVA", kVA_sum)
+	
+	kWh = readESMkWh1()
+	influx_write("Meter 1", "kWh", kWh)
+	v_max_min(v, v_min, v_max)
+	sec_counter += 1
+	
+	# Averages 1
+	averages("1", kW_avg, kW_sum, "Avg kW")
+	averages("1", kVA_avg, kVA_sum, "Avg kVA")
+	averages("1", V_avg, volt_sum, "Avg V")
+	
+	
+	# Meter 2
 	kW = readESMkW2()
-	kWSum = kWSum + kW
-	print(kWSum) 
-	influx_write("Modbus_D", "PM311", "kW", kWSum)
-	time.sleep(2)
-	'''
-	V = int(readESMV2()/10)
-	VoltageSum = VoltageSum + V
-
+	kW_sum2 += kW
+	influx_write("Meter 1", "kW", kW_sum)
+	
+	v = int(readESMV2()/10)
+	volt_sum2 += v
+	influx_write("Meter 2", "V", volt_sum)
+	
 	kVA = readESMkVA2()
-	kVASum = kVASum + kVA
-
+	kVA_sum2 += kVA
+	influx_write("Meter 2", "kVA", kVA_sum)
+	
 	kWh = readESMkWh2()
-	if (V > VMax):
-		VMax = V
-	if (V < VMin):
-		VMin = V
-		time.sleep(1) 				# sleep 1 second
-	SecondCounter+=1
-	if (SecondCounter >= AveragingInterval) :
-		AveragekW = kWSum / SecondCounter
-		AveragekVA = kVASum / SecondCounter
-		AverageVoltage = VoltageSum / SecondCounter
-		#writeToAnarduino(AverageVoltage, VMax, VMin, AveragekW, AveragekVA, kWh)
-		kWSum = 0
-		kVASum = 0
-		VoltageSum = 0
-		SecondCounter = 0
-		VMax = -1 				# set unreachably low value for VMax for next averaging period
-		VMin = 100000 			# set unreachably high value for VMin for next averaging period
-	'''
-			
-
-	except Exception as inst:
-		print type(inst)
-		print inst
-		print "Communications error.  Continuing"
-		time.sleep(5)
-		# Reset averaging
-		kWSum = 0
-		kVASum = 0
-		VoltageSum = 0
-		SecondCounter = 0
-		VMax = -1
-		VMin = 100000
+	influx_write("Meter 2", "kWh", kWh)
+	
+	v_max_min(v, v_min, v_max)
+	sec_counter += 1
+	
+	# Averages 2
+	averages("2", kW_avg, kW_sum2, "Avg kW")
+	averages("2", kVA_avg, kVA_sum2, "Avg kVA")
+	averages("2", V_avg, volt_sum2, "Avg V")
+	
+	time.sleep(0.02)						# read every 20ms
